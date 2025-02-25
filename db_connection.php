c2da3ecbe6a3417123680a8d4e4ca2620b450db4
CarlosFC123 <carlosbalam496@gmail.com>
2025-02-25 17:24:32 -0600

    first commit

--- db_connection.php
+++ db_connection.php
@@ -3,7 +3,7 @@ $host = getenv('DB_HOST') ?: "localhost";
 $dbname = getenv('DB_NAME') ?: "cafeteria";
 $username = getenv('DB_USER') ?: "root";
 $password = getenv('DB_PASSWORD') ?: "";
-
+//mysql://root:QqsqcnIwVSSruFsbxwJUnCBNXRZCIeEq@crossover.proxy.rlwy.net:31168/railway
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
